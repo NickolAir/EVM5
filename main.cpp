30fa63046e73413115468cf233b0868f04d6ba09
Nikolay Ratushnyak <112893665+NickolAir@users.noreply.github.com>
2022-12-05 20:42:41 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,6 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	Ptr<BackgroundSubtractor> pBackSub;
-	pBackSub = createBackgroundSubtractorKNN();
-
 	//Open the default video camera
 	VideoCapture cap(0);
 
@@ -21,18 +18,20 @@ int main(int argc, char* argv[])
 
 	// Number of frames to capture
 	int num_frames = 1;
+	int fps_count = 1;
 
 	// Start and end times
-	clock_t start;
-	clock_t end;
+	clock_t fps_start, start_capture, start_processing, full_start;
+	clock_t fps_end, end_capture, end_processing, full_end;
 
-	Mat frame, fgMask;
+	Mat frame;
 
 	cout << "Capturing " << num_frames << " frames" << endl;
 
-	double ms, fpsLive;
-
-	int keyboard;
+	double fpsLive = 1;
+	double total_capture_time = 0;
+	double processing_time = 0;
+	double capture_time = 0;
 
 	// if not success, exit program
 	if (cap.isOpened() == false)
@@ -49,12 +48,16 @@ int main(int argc, char* argv[])
 
 	string window_name = "My Camera Feed";
 	namedWindow(window_name); //create a window called "My Camera Feed"
-
+	fps_start = clock();
+	full_start = clock();
 	while (true)
 	{
-		Mat frame, fgMask;
+		Mat frame;
+		start_capture = clock();
 		bool bSuccess = cap.read(frame); // read a new frame from video 
-		
+		end_capture = clock();
+		capture_time += (double(end_capture) - double(start_capture)) / double(CLOCKS_PER_SEC);
+
 		//Breaking the while loop if the frames cannot be captured
 		if (bSuccess == false)
 		{
@@ -63,28 +66,21 @@ int main(int argc, char* argv[])
 			break;
 		}
 
-		start = clock();
+		start_capture = clock();
 		cap >> frame;
-		pBackSub->apply(frame, fgMask);
-		long sum = 0;
-		int N = 1;
+		end_capture = clock();
+		capture_time += (double(end_capture) - double(start_capture)) / double(CLOCKS_PER_SEC);
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				sum += 1;
-			}
-		}
-		end = clock();
-		double seconds = (double(end) - double(start)) / double(CLOCKS_PER_SEC);
-		cout << "Time taken : " << seconds << " seconds" << endl;
-		fpsLive = double(num_frames) / double(seconds);
-		cout << "Estimated frames per second : " << fpsLive << endl;
+		num_frames += 1;
 
+		start_processing = clock();
 		//cvtColor(frame, frame, COLOR_BGR2HSV, 1); //CHANGE COLOR MODEL
-		//cvtColor(frame, frame, COLORMAP_PINK, 1); //GREY FILTER
-		frame.convertTo(frame, -1, 0.5, 0); //decrease the contrast by 0.5
+		cvtColor(frame, frame, COLORMAP_PINK, 1); //GREY FILTER
+		//frame.convertTo(frame, -1, 0.5, 0); //decrease the contrast by 0.5
 		//blur(frame, frame, Size(25, 25));
-		
+		end_processing = clock();
+		processing_time += (double(end_processing) - double(start_processing)) / double(CLOCKS_PER_SEC);
+
 		putText(frame, "FPS: " + to_string(fpsLive), { 50, 50 }, FONT_HERSHEY_COMPLEX, 1.5, (255, 255, 255));
 		//show the frame in the created window
 		imshow(window_name, frame);
@@ -98,7 +94,27 @@ int main(int argc, char* argv[])
 			cout << "Esc key is pressed by user. Stoppig the video" << endl;
 			break;
 		}
+
+		fps_end = clock();
+		double seconds = (double(fps_end) - double(fps_start)) / double(CLOCKS_PER_SEC);
+
+		if (seconds >= 1) {
+			fps_start = fps_end;
+			cout << "Time taken : " << seconds << " seconds" << endl;
+			fpsLive = double(num_frames) / double(seconds);
+			cout << "Estimated frames per second : " << fpsLive << endl;
+			fps_count += num_frames;
+			num_frames = 0;
+		}
 	}
+	full_end = clock();
+	total_capture_time = (full_end - full_start) / double(CLOCKS_PER_SEC);
+	cout << "Total capture time " << total_capture_time << endl;
+	cout << "Processing time " << processing_time << endl;
+	cout << "Capture frame time " << capture_time << endl;
+	cout << "Percent processing time " << processing_time / total_capture_time * 100 << endl;
+	cout << "Percent of capture frame time " << capture_time / total_capture_time * 100 << endl;
+	cout << "Frames in total " << fps_count << endl;
 	cap.release();//Releasing the buffer memory
 	return 0;
 }
